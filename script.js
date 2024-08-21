1699c3bec70c6f108f2ef00a91bd6dad282c8de6
mook1122 <kmuk223@gmail.com>
2024-08-21 19:29:23 +0900

    이미지 추가 & UI작업 완료

--- script.js
+++ script.js
@@ -23,11 +23,40 @@ menuItems.forEach(item => {
 });
 
 
+const mobileMenuIcon = document.querySelector('.mobile-header-menu');
+const closeMenu = document.querySelector('.close-mobile-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const mobileMenuItems = document.querySelectorAll('.mobile-menu-list li');
+
+mobileMenuIcon.addEventListener('click', function () {
+    mobileMenu.classList.add('open');
+});
+
+closeMenu.addEventListener('click', function () {
+    mobileMenu.classList.remove('open');
+});
+
+
+mobileMenuItems.forEach(item => {
+    item.addEventListener('click', function () {
+        const targetId = this.getAttribute('data-target');
+
+        mobileMenu.classList.remove('open');
+
+        rightPageItems.forEach(pageItem => {
+            if (pageItem.id === targetId) {
+                pageItem.classList.add('active');
+            } else {
+                pageItem.classList.remove('active');
+            }
+        });
+    });
+});
+
+
+
 document.getElementById('move-about').addEventListener('click', () => {
 
-
-    console.log(123);
-    
     rightPageItems.forEach(pageItem => {
         if (pageItem.id === 'about') {
             pageItem.classList.add('active');
